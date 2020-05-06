37fe412200b6112a564ebbadddd4c1ac1f4a2c23
Jolie Guerra <Jolie_guerra@yahoo.com>
2020-05-06 13:48:32 -0400

    added project file

---
+++ src/components/Project/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Project(props) {
+  return (
+    <div className="col-md-6 col-12">
+      <div className="card">
+        <img className="card-img-top" src={props.image} alt={props.name} />
+        <div className="card-body">
+          <h5 className="card-title">{props.name}</h5>
+          <a
+            href={props.site}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="card-link"
+          >
+            Go To App
+          </a>
+          <a
+            href={props.github}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="card-link"
+          >
+            Github Repo
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
