6ebd21d2f801259793d04e98f7dff5afb02854ee
Jolie Guerra <Jolie_guerra@yahoo.com>
2020-07-02 11:12:00 -0400

    Updates

---
+++ precache-manifest.2f05f8cc5538fc320352278f326870be.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba99be417bf0b33852207e6cacd79a56",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "41c60bbaf615997d26ae",
+    "url": "/react-portfolio/static/css/main.9a78cee5.chunk.css"
+  },
+  {
+    "revision": "577ddad638897b5efb45",
+    "url": "/react-portfolio/static/js/2.2a528c80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.2a528c80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c60bbaf615997d26ae",
+    "url": "/react-portfolio/static/js/main.de106708.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "c9aad77fc5eba6b26e354a7a5921ba1f",
+    "url": "/react-portfolio/static/media/BudgetTracker.c9aad77f.jpeg"
+  },
+  {
+    "revision": "fae06be9c5056673115c2d4f07026013",
+    "url": "/react-portfolio/static/media/EatDaBurgerScreenshot.fae06be9.jpeg"
+  },
+  {
+    "revision": "0075791537b2ddeea8e3cf25643aa6ea",
+    "url": "/react-portfolio/static/media/FitnessTracker.00757915.jpeg"
+  },
+  {
+    "revision": "db6a3126e4f045e870ead18ccf79e686",
+    "url": "/react-portfolio/static/media/NoteTaker.db6a3126.jpeg"
+  },
+  {
+    "revision": "7e357e212fb46f2ec7bbdd90c7f22b0e",
+    "url": "/react-portfolio/static/media/Project-2.7e357e21.jpeg"
+  },
+  {
+    "revision": "fd2ae8ece432d3ba5057da879104d54e",
+    "url": "/react-portfolio/static/media/Project2.fd2ae8ec.jpeg"
+  },
+  {
+    "revision": "b81c592c79c982dbc642e9c39d922672",
+    "url": "/react-portfolio/static/media/main1.b81c592c.jpeg"
+  }
+]);
